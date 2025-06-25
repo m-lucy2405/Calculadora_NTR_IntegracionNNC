73dd35ddcf625a722a28695b30dc0d61ddca85fa
josuehdz420 <josuehdz420@gmail.com>
2025-06-24 23:12:00 -0600

    Ondas de Documentación actualizadas (Integración/Newton)

--- numerical_tool_ANS/numerical_tool/core/urls.py
+++ numerical_tool_ANS/numerical_tool/core/urls.py
@@ -5,4 +5,6 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('documentacion-integracion/', views.documentacion_integracion, name='documentacion_integracion'),
+    path('documentacion-newton/', views.documentacion_newton, name='documentacion_newton'),
 ]
